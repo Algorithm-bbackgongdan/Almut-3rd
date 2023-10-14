dae78927fe5aaad464aa75d0d1537184e5ad9493
itsme-shawn <chs98105@gmail.com>
2023-10-14 23:57:52 +0900

    README 작성

--- code/itsme-shawn/week2/boj_2573.py
+++ code/itsme-shawn/week2/boj_2573.py
@@ -12,18 +12,18 @@ def bfs(y, x):
     visited[y][x] = True
 
     while queue:
-        r, c = queue.popleft()
+        r, c = queue.popleft() # row, col
         for dy, dx in directions:
-            dr = r + dy
-            dc = c + dx
+            nr = r + dy # next row
+            nc = c + dx # next col
 
-            if 0 <= dr < n and 0 <= dc < m:
-                if iceberg[dr][dc] == 0:
-                    visited[r][c] += 1
-
-                if not visited[dr][dc] and iceberg[dr][dc] != 0:
-                    queue.append((dr, dc))
-                    visited[dr][dc] = True
+            if 0 <= nr < n and 0 <= nc < m:
+                if iceberg[nr][nc] == 0: # 주변이 빙산이 아니라면 (바다라면)
+                    visited[r][c] += 1 # 깎을 빙산 수 증가
+ 
+                if not visited[nr][nc] and iceberg[nr][nc] != 0: # 미방문한 빙산에 대해
+                    queue.append((nr, nc))
+                    visited[nr][nc] = True
 
 time = 0
 while True:
@@ -33,8 +33,9 @@ while True:
         for j in range(m):
             if not visited[i][j] and iceberg[i][j] != 0:
                 bfs(i, j)
-                count += 1
+                count += 1 # 빙산 개수 증가
 
+    # bfs 다 돌린 후 빙산의 높이 감소 시키기
     for i in range(n):
         for j in range(m):
             if visited[i][j]:
@@ -43,7 +44,7 @@ while True:
                     iceberg[i][j] = 0
 
     time += 1
-    if count == 0:
+    if count == 0: # 분리된 빙산이 없는 경우
         break
     if count >= 2:
         flag = True
