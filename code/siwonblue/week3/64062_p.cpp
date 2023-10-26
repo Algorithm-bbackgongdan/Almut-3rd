f1db6ce7797db0f8feac89473f7bb61ac8d15c8e
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-10-26 15:46:42 +0900

    [feat] 64062_p.cpp done

--- code/siwonblue/week3/64062_p.cpp
+++ code/siwonblue/week3/64062_p.cpp
@@ -5,58 +5,53 @@
 
 using namespace std;
 
-vector<int> cross_bridge(vector<int> stones,int mid){
-  vector<int> deep_copy_stones = stones; 
-  for(int i=0;i<deep_copy_stones.size();i++){
-    deep_copy_stones[i] = deep_copy_stones[i] - mid <0 ? 0 : deep_copy_stones[i] - mid ;
-  };
-  return deep_copy_stones;
-};
-
-int check(vector<int> stones, int k){
-  int sum = 0; // 2* 10**8 * 2 * 10**5 
-  int cnt = 0;
+bool check(vector<int> stones, int mid, int k){
+  int sum = 0; 
+  int block = 0;
   
-  for(int i=0;i<k;i++) sum+=stones[i];
-  if(sum==0) cnt+=1;
-  for(int i=k;i<stones.size();i++){
-    sum += stones[i] - stones[i-k];
-    if(sum==0) cnt+=1;
+  // (mid-1) people cross bridge
+  vector<int> deep_copy_stones = stones;
+  for(int i=0;i<deep_copy_stones.size();i++){
+    deep_copy_stones[i] = deep_copy_stones[i] - (mid-1)<0 ? 0 : deep_copy_stones[i] - (mid-1);
   };
-  if (cnt<1)  return 0;
-  if (cnt==1) return 1;
-  else return 2;
+
+  // for(int i=0;i<deep_copy_stones.size();i++) cout  << deep_copy_stones[i] << " ";
+  
+  // check if mid people can cross bridge
+  for(int i=0;i<k;i++) sum+=deep_copy_stones[i];
+  if(sum==0) block+=1;
+  for(int i=k;i<deep_copy_stones.size();i++){
+    sum += deep_copy_stones[i] - deep_copy_stones[i-k];
+    if(sum==0) block+=1;
+  };
+  if (block>=1)  return false; // cannot pass
+  else return true; // can pass
 };
 
 
 int search(vector<int> stones, int high, int k){
-  int start = 1;
+  int start = 0;
   int end = high;
   int ans = -1;
-  int cnt;
-  vector<int> deep_copy_stones;
+  bool can_pass;
   while(start<=end){
     int mid = (start+end)/2;
     
-    deep_copy_stones = cross_bridge(stones,mid);
+    
     // cout << "start:" << start<<" ";
     // cout << "end:" << end<<" ";
     // cout << "mid:" << mid <<" "<< "\n";
 
-    // for(int i=0;i<deep_copy_stones.size();i++) cout << deep_copy_stones[i] << " ";
-    cnt = check(deep_copy_stones, k); // parametric search
+    can_pass = check(stones,mid, k); // parametric search
     // cout << "\n" << "\n";
     
-    if(cnt==0){
+    if(can_pass){
+      ans = max(ans, mid);
       start = mid + 1;
-    }else if(cnt==1){
-      ans = mid;
-      break;
     }else{
       end = mid - 1;
     }
-
-  }
+  };  
   return ans;
 };
 
